2cc38819795b5b558a770dd5a035b0319c9b3ad2
sakatashi <shiori.1002sakata@gmail.com>
2022-12-20 07:58:57 +0000

    [Add]myage

---
+++ myAge.java
@@ -0,0 +1,8 @@
+public class myAge {
+  public static void main (String[] arges) {
+    int age = 20;
+    System.out.println("私の年齢は" + age + "です");
+    age = 25;
+    System.out.println("いや、本当の年齢は" + age + "です");
+  }
+}
