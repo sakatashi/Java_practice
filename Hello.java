3f36a3bd02f8ccbab48a6c7d173db2db7ed1bbcd
sakatashi <shiori.1002sakata@gmail.com>
2022-12-21 08:12:18 +0000

    [Add]hello

---
+++ Hello.java
@@ -0,0 +1,8 @@
+public class Hello {
+  public static void main (String [] args) {
+    String name = "すがわら";
+    String message;
+    message = name + "さん、こんにちは";
+    System.out.println(message);
+  }
+}
