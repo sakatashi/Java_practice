8e1a95901f3ec0e42005eefe4a9bfc6149aa2783
sakatashi <shiori.1002sakata@gmail.com>
2022-12-23 14:42:05 +0000

    [Add]while

---
+++ Toilet.java
@@ -0,0 +1,9 @@
+public class Toilet {
+  public static void main(String[] args) {
+    boolean doorClose = true;    // ここでtrueかfalseを代入
+    while (doorClose == true) {
+      System.out.println("ノックする");
+      System.out.println("1分待つ");
+    }         
+  }             
+}
