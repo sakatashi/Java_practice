da1a739b5d485d3dad87a5a5d70813fc59cf585d
sakatashi <shiori.1002sakata@gmail.com>
2022-12-25 14:32:44 +0000

    [Add]tenki2

---
+++ Tenki2.java
@@ -0,0 +1,12 @@
+public class Tenki2 {
+  public static void main(String[] args) {
+    boolean tenki = false;
+    if (tenki == true) {
+      System.out.println("洗濯をします");
+      System.out.println("散歩にいきます");
+    } else {
+      System.out.println("DVDを見ます");
+      System.out.println("寝ます");
+    }
+  }
+}
