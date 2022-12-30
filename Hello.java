858452842d432381b33a16d68bfe4470f1c6f47c
sakatashi <shiori.1002sakata@gmail.com>
2022-12-30 14:30:20 +0000

    [Add]hello

---
+++ Hello.java
@@ -0,0 +1,10 @@
+public class Hello {
+  public static void main(String[] args) {
+    System.out.println("メソッドを呼び出します");
+    hello();
+    System.out.println("メソッドの呼び出しが終わりました");
+  }
+  public static void hello() {
+    System.out.println("湊さん、こんにちは");
+  }
+}
