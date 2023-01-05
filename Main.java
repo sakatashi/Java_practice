32868cb3bb17587d716aa5382d247c2da3dc9c00
sakatashi <shiori.1002sakata@gmail.com>
2023-01-05 11:58:43 +0000

    [Add]matango

--- Main.java
+++ Main.java
@@ -1,15 +1,21 @@
 public class Main {
   public static void main(String[] args) {
-    // 1.勇者を生成
     Hero h = new Hero();
-    // 2.フィールドに初期値をセット
     h.name = "ミナト";
     h.hp = 100;
-    System.out.println("勇者" + h.name + "を生み出しました！");
-    // 3.勇者のメソッドを呼び出してゆく
-    h.sit(5);
+
+    Matango m1 = new Matango();
+    m1.hp = 50;
+    m1.suffix = 'A';
+
+    Matango m2 = new Matango();
+    m2.hp = 48;
+    m2.suffix = 'B';
+
+    // 冒険のはじまり
     h.slip();
-    h.sit(25);
+    m1.run();
+    m2.run();
     h.run();
   }
 }
